37cf59d3cbfee53257756d895389c78243c3e88e
yadi09 <yadamzerterefe09@gmail.com>
2024-03-04 09:20:12 +0100

    ..

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ *
+ **/
+
+void bubble_sort(int *array, size_t size)
+{
+  int i, j, temp;
+
+  if (size < 2)
+    return;
+
+  for (i = size - 1; i > 0; i--)
+    {
+      for (j = 0; j < i; j++)
+	{
+	  if (array[j] > array[j + 1])
+	    {
+	      temp = array[j];
+	      array[j] = array[j + 1];
+	      array[j + 1] = temp;
+	      print_array(array, size);
+	    }
+	}
+    }
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,7 +20,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+void bubble_sort(int *array, size_t size);
 
 
 
